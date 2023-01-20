524973a8833bc3aeec4bf5cf63f600f6af8a4b04
qapush <mihail.kapush@gmail.com>
2023-01-20 11:32:59 +0100

    Add github pages deploy functionality

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,8 +6,10 @@ export default class Header extends Component
     render(){
         return(
             <>
-                <h1>Kurwafikator</h1>
-                <hr />
+                <div className="header">
+                    <h1>Kurwafikator</h1>
+                    <hr />
+                </div> 
             </>
         )
     }
